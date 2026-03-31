acsone/module-composition-analysis

[REF] Allow same module name across different repositories
This change allows to get a module `x` present in different
repositories (unicity constraint on `odoo.module.branch` has changed
accordingly). This is useful in case two projects have `x` installed but
the code of these modules (that can be different) are hosted in their
own project repository.

Therefore, the scan of repositories and project modules import have been
adapted to fullfil this new possibility.

Features:
    - allows multiple instances of `<odoo.module.branch>` records
      sharing the same module technical name and branch but belonging to
      different repositories,
    - repositories have now a new `specific` flag, propagated to their
      modules.. By default they host generic modules, but project repositories
      will host specific modules that cannot be shared to other projects,
    - a generic module cannot depend on a specific module
    - when importing modules in a project, specific modules of other
      projects cannot be mapped,
    - once a specific module is scanned in a project repository, relevant
      orphaned modules installed in a project will be re-mapped to this newly
      created specific module,
    - detect PR only on generic modules (unmerged/pending modules are
      generic only, specific modules have to be found in project
      repositories).

odoo_project/models/odoo_module_branch.py
@@ -26,3 +26,32 @@ class OdooModuleBranch(models.Model):
     def _compute_module_ids(self):
         for rec in self:
             rec.odoo_project_ids = rec.odoo_project_module_ids.odoo_project_id.ids
+
+    def _filter_module_to_update(self, repo_branch, module_branch):
+        # A module found in a specific repository cannot be linked to an orphaned
+        # module that is already listed/installed in different projects (chances
+        # are these projects refer to a generic version of the module).
+        # For such specific module we want to create a dedicated 'odoo.module.branch'.
+        if (
+            repo_branch.repository_id.specific
+            # Orphaned module
+            and module_branch
+            and not module_branch.repository_id
+            # Installed in multiple projects
+            # NOTE: we could have multiple project instances using the same
+            # repository, so we check the underlying repo instead
+            and len(module_branch.project_ids.repository_id) > 1
+        ):
+            repo_projects = repo_branch.repository_id.project_ids
+            if repo_projects:
+                # Corner case: if the current project(s) are referring to this
+                # orphaned module, remove them as they will now have a dedicated
+                # specific module created.
+                # Spawn a job to re-map project modules to the new (not yet created)
+                # specific module.
+                project_modules = module_branch.odoo_project_module_ids.filtered(
+                    lambda o: o.odoo_project_id in repo_projects
+                )
+                project_modules.with_delay()._remap_to_specific_module()
+            return False
+        return module_branch

akretion/odoo-repository

odoo_project/models/odoo_module_branch.py
@@ -26,3 +26,32 @@ class OdooModuleBranch(models.Model):
     def _compute_module_ids(self):
         for rec in self:
             rec.odoo_project_ids = rec.odoo_project_module_ids.odoo_project_id.ids
+
+    def _filter_module_to_update(self, repo_branch, module_branch):
+        # A module found in a specific repository cannot be linked to an orphaned
+        # module that is already listed/installed in different projects (chances
+        # are these projects refer to a generic version of the module).
+        # For such specific module we want to create a dedicated 'odoo.module.branch'.
+        if (
+            repo_branch.repository_id.specific
+            # Orphaned module
+            and module_branch
+            and not module_branch.repository_id
+            # Installed in multiple projects
+            # NOTE: we could have multiple project instances using the same
+            # repository, so we check the underlying repo instead
+            and len(module_branch.odoo_project_ids.repository_id) > 1
+        ):
+            repo_projects = repo_branch.repository_id.project_ids
+            if repo_projects:
+                # Corner case: if the current project(s) are referring to this
+                # orphaned module, remove them as they will now have a dedicated
+                # specific module created.
+                # Spawn a job to re-map project modules to the new (not yet created)
+                # specific module.
+                project_modules = module_branch.odoo_project_module_ids.filtered(
+                    lambda o: o.odoo_project_id in repo_projects
+                )
+                project_modules.with_delay()._remap_to_specific_module()
+            return False
+        return module_branch