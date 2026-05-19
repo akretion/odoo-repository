c7b79aaf9c9bb2169f21ea9244e6595c2fa309d6
Francois Poizat <francois.poizat@gmail.com>
2026-05-19 12:48:06 +0200

    Add 'odoo_project_migration_api'

---
+++ odoo_project_migration_api/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Odoo Project Migration Api",
+    "summary": "Generate migration spec.yaml from older project version",
+    "version": "18.0.1.0.0",
+    "category": "Tools",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/module-composition-analysis",
+    "data": [],
+    "installable": True,
+    "depends": [
+        "odoo_project",
+        "odoo_repository_migration",
+    ],
+    "license": "AGPL-3",
+}
